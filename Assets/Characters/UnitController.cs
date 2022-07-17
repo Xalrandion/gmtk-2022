89e1dd40ad21fe8ecf6aa6b193d7f9a40a51195a
Maxime Tarel <maxime.tarel@outlook.fr>
2022-07-17 07:24:50 +0200

    [ASSET] add BoardScene

--- Assets/Characters/UnitController.cs
+++ Assets/Characters/UnitController.cs
@@ -8,6 +8,7 @@ public class UnitController : MonoBehaviour
 {
     public Animator? anime;
     public UnityEvent<Vector3> onAttack = new();
+    public Transform? target;
 
     void Start()
     {
@@ -16,6 +17,14 @@ public class UnitController : MonoBehaviour
         }
     }
 
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Attack(target!.position);
+        }
+        if (Input.GetMouseButtonDown(1)) {
+            Defeat();
+        }
+    }
     void Attack(Vector3 Target) {
         anime?.Play("Attack");
         onAttack?.Invoke(Target);
